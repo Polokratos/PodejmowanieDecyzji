8c13bfd84153f2ec9d9f4c7b0cf410980ac396e4
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-12-25 15:27:31 +0100

    scale fix

--- web/components/SurveyDrawer/SurveyBodyComponent.tsx
+++ web/components/SurveyDrawer/SurveyBodyComponent.tsx
@@ -14,7 +14,11 @@ export const SurveyBodyComponent = (props : SurveyBodyProps) : JSX.Element => {
     
     const {currentQuestion: question,surveyContext,next,previous,answer,setAnswer} = props;
 
-    const mapAnswer = (answer:number) => Math.abs(answer)+1;
+    const mapAnswerToUi = (answer:number) => Math.abs(answer)+1;
+    const mapAnswerToServer = (answer:number) => {
+        if(answer >= 0) return answer +1;
+        return 1/(-answer+1);
+    }
 
     const QuestionContainer = (
         <>
@@ -34,10 +38,10 @@ export const SurveyBodyComponent = (props : SurveyBodyProps) : JSX.Element => {
             <div className="drawer-input-container">
                 <div className="drawer-body-row-sb">
                     <a>9x</a><a>7x</a><a>5x</a><a>3x</a>
-                    <a>{mapAnswer(answer)}</a>
+                    <a>{mapAnswerToUi(answer)}</a>
                     <a>3x</a><a>5x</a><a>7x</a><a>9x</a>
                 </div>
-                <input type="range" min="-8" max="8" value={answer} onChange={(e) => {setAnswer(Number.parseFloat(e.target.value));}}/>
+                <input type="range" min="-8" max="8" value={answer} onChange={(e) => {setAnswer(mapAnswerToServer(Number.parseFloat(e.target.value)));}}/>
             </div>
             <button className="drawer-body-button" onClick={next}>{"Next"}</button>
         </div>
